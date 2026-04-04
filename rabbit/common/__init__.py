90b1f8f2da4637973cdff78904a9557f5bf2cd91
vkhehay <v.khehay@gmail.com>
2026-04-04 21:49:34 +0300

    created dead letter queue

--- rabbit/common/__init__.py
+++ rabbit/common/__init__.py
@@ -1,6 +1,9 @@
 from .email_exchanger import EmailUpdatesRabbit, EmailUpdatesRabbitMixin
+from .simple_exchanger import SimpleRabbit, SimpleRabbitMixin
 
 __all__ = [
     "EmailUpdatesRabbit",
     "EmailUpdatesRabbitMixin",
+    "SimpleRabbit",
+    "SimpleRabbitMixin",
 ]
---
+++ rabbit/common/simple_exchanger.py
@@ -0,0 +1,79 @@
+"""
+- declare exchange for email....
+- bind queue
+- start consumer messaging
+"""
+
+import logging
+
+import rabbit.RMQ_pika_config as Config
+from typing import TYPE_CHECKING, Callable
+from pika.exchange_type import ExchangeType
+
+from rabbit.base import RabbitBase
+
+if TYPE_CHECKING:
+    from pika.adapters.blocking_connection import BlockingChannel
+    from pika.spec import (
+        Basic,
+        BasicProperties,
+    )
+
+log = logging.getLogger(__name__)
+
+
+class SimpleRabbitMixin:
+    channel: "BlockingChannel"
+
+    def declare_queue(self) -> None:
+        # fanout запускает все во все очереди и dlq сообщения попадают в рабочий queue
+        # self.channel.exchange_declare(
+        #     exchange=Config.RMQ_DEAD_LETTER_EXCHANGE,
+        #     exchange_type=ExchangeType.fanout,
+        # )
+        dlq = self.channel.queue_declare(
+            queue=Config.RMQ_DEAD_LETTER_KEY,
+            durable=True,
+        )
+        self.channel.queue_bind(
+            queue=dlq.method.queue,
+            exchange=Config.RMQ_DEAD_LETTER_EXCHANGE,
+        )
+        log.info("Created dlq: %s", dlq.method.queue)
+        queue = self.channel.queue_declare(
+            queue=Config.RMQ_ROUTING_KEY,
+            durable=True,
+            arguments={
+                "x-dead-letter-exchange": Config.RMQ_EXCHANGE,
+                # "x-dead-letter-routing-key": Config.RMQ_DEAD_LETTER_KEY,
+            }
+
+        )
+        log.info("Declare Queue: %s", queue.method.queue)
+
+    def consume_messages(
+        self,
+        message_callback: Callable[
+            [
+                "BlockingChannel",
+                "Basic.Deliver",
+                "BasicProperties",
+                bytes,
+            ],
+            None,
+        ],
+        prefetch_count: int = 1,
+    ):
+        self.channel.basic_qos(prefetch_count=prefetch_count)
+        self.declare_queue()
+        self.channel.basic_consume(
+            queue=Config.RMQ_ROUTING_KEY,
+            on_message_callback=message_callback,
+            # auto_ack = True,
+        )
+        log.warning("Waiting for messages")
+        self.channel.start_consuming()
+
+
+class SimpleRabbit(SimpleRabbitMixin, RabbitBase):
+    pass
