4df479d60f0c9dd2e2fec988ed0a209c8ae7562e
vkhehay <v.khehay@gmail.com>
2026-04-09 10:42:52 +0300

    fixed bugs

--- proj6/utils_aiosmptlib_web/send_welcome_email.py
+++ proj6/utils_aiosmptlib_web/send_welcome_email.py
@@ -7,8 +7,15 @@ from core import User
 async def send_welcome_email(
     user: User,
 ):
-    topic = f"Welcome, {user.user_data.name}"
-    letter_body = f"Dear, {user.user_data.name}, \n\nThank you for choosing our service"
+    name = user.user_data.name
+    topic = ""
+    if not name:
+        topic = f"Welcome, {user.user_data.last_name}"
+        name = user.user_data.last_name
+    elif not user.user_data.last_name:
+        topic = f"Welcome, Sir/Madam"
+        name = "Sirs"
+    letter_body = f"Dear, {name}, \n\nThank you for choosing our service"
 
     await send_email(
         recipient=user.email,
